5b558fa7bafebe29c0c06a7deb7f0c9b2a1b49a4
Moritz Wassmer <moritz@wassmer-bw.de>
2023-11-28 22:30:43 +0100

    added match_by_bucket etc to matching.py

--- matching.py
+++ matching.py
@@ -1,7 +1,10 @@
 # Blocking, matching
 import Levenshtein
+import pandas as pd
+from tqdm import tqdm
+from itertools import combinations, product
 
-def similar_score_str(str1, str2):
+def similar_score_str(str1:str, str2:str):
 
     """
     compare 2 strings based on Levenshtein distance
@@ -16,7 +19,7 @@ def similar_score_str(str1, str2):
     
     return similarity
 
-def total_similarity(row1, row2):
+def total_similarity(row1:pd.Series, row2:pd.Series):
 
     """
     averaged similarity based on 3 attributes
@@ -28,7 +31,7 @@ def total_similarity(row1, row2):
     year_sim = row1["Year"] == row2["Year"]
     return ( title_sim + venue_sim + year_sim)/3
 
-def exact_match(row1, row2): # not used
+def exact_match(row1:pd.Series, row2:pd.Series): # not used
 
     """
     exact match of attributes
@@ -39,3 +42,61 @@ def exact_match(row1, row2): # not used
     venue_sim = row1["Venue"] == row2["Venue"]
     year_sim = row1["Year"] == row2["Year"]
     return title_sim & venue_sim & year_sim
+
+
+def match_by_bucket(df1:pd.DataFrame, df2:pd.DataFrame, similarity_function, threshold:float=1):
+
+    """
+    matches the rows by using the similarity funciton. 
+
+    Return:
+        List of tuples of indices [(a,b), (a,c)]
+    """
+
+    matched_ids = list()
+    
+    unique_buckets = set(df1["bucket"]).union(set(df2["bucket"]))
+    
+    iterations = 0
+    
+    for bucket in tqdm(unique_buckets):
+
+        df1_bucket = df1[df1['bucket'] == bucket].reset_index()
+        df2_bucket = df2[df2['bucket'] == bucket].reset_index()
+
+        for ix1, ix2 in product(list(df1_bucket.index),list(df2_bucket.index)): # each combination
+
+            row1 = df1_bucket.iloc[ix1]
+            row2 = df2_bucket.iloc[ix2]
+
+            if similarity_function(row1, row2) >= threshold: # Hardcoded threshold
+                matched_ids += [(row1["Index"], row2["Index"])]
+                
+            iterations += 1
+                     
+    return matched_ids, iterations #+ list(unmatched)
+
+def match_without_bucket(df1:pd.DataFrame, df2:pd.DataFrame, similarity_function, threshold:float=1):
+
+    """
+    matches the rows by using the similarity funciton without using the bucket column in the dataframe 
+
+    Return:
+        List of tuples of indices [(a,b), (a,c)]
+    """
+
+    combinations = list(product(list(df1.index),list(df2.index)))
+    matched_ids = list()
+    iterations = 0
+    
+    for ix1, ix2 in tqdm(combinations): # each combination
+
+        row1 = df1.iloc[ix1]
+        row2 = df2.iloc[ix2]
+
+        if similarity_function(row1, row2) >= threshold: # Hardcoded threshold
+            matched_ids += [(row1["Index"], row2["Index"])]
+        
+        iterations += 1
+
+    return matched_ids, iterations #+ list(unmatched)
