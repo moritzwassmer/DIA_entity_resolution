97f1509bec4536c87c3d4a89614fc9e06b8abfc2
Moritz Wassmer <moritz@wassmer-bw.de>
2023-12-15 20:27:43 +0100

    first running version of modularized spark pipeline

--- matching.py
+++ matching.py
@@ -137,7 +137,7 @@ def get_unmatched(matched_pairs, unmatched_pairs): # TODO what is this? still ne
     return unique_strings_unmatched
 """
 
-def matching_spark(df1, df2, similarity_expression, threshold, sfx_1="_acm", sfx_2="_dblp"):
+def matching_spark(df1, df2, similarity_expression, threshold=1, sfx_1="_acm", sfx_2="_dblp"):
 
     # 1) Add suffixes "_acm" and "_dblp" to the column names before join
     df1 = df1.select(
